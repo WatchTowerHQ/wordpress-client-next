2592807009f5e3cc793c06f7c2b467b71c3cd4a3
C2P <office@code2prog.com>
2025-04-28 11:26:49 +0300

    Increase database records limit to 6500.

--- watchtowerhq.php
+++ watchtowerhq.php
@@ -38,7 +38,7 @@ define('WHTHQ_BACKUP_DIR_NAME', 'watchtower_backups');
 define('WHTHQ_BACKUP_EXCLUSIONS_ENDPOINT', '/backupExclusions');
 define('WHTHQ_BACKUP_DIR', wp_upload_dir()['basedir'] . '/' . WHTHQ_BACKUP_DIR_NAME);
 define('WHTHQ_BACKUP_FILES_PER_QUEUE', class_exists("ZipArchive") ? 450 : 200);
-define('WHTHQ_DB_RECORDS_MAX', 6000);
+define('WHTHQ_DB_RECORDS_MAX', 6500);
 define('WHTHQ_BRANDING_FILE', wp_upload_dir()['basedir'] . '/' . 'WatchTowerClientCustomBranding.json');
 define('WHTHQ_MAX_HEADQUARTER_IDLE_TIME_SECONDS', 172800);
 
